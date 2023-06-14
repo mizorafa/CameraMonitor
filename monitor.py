62031687aa5e6da2e52b0fa44ddb1ace01e6a648
Shotaro <shotaro@icrr.u-tokyo.ac.jp>
2023-06-14 14:31:51 +0100

    initial files

---
+++ monitor.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+"""
+ ---- 13th June, 2023 ----
+Developed by S. Abe, using a ClusCo log reader that Seiya developed.
+usage: 
+put this script with "sclusco.py", change "path_clog" in main(), and run "python monitor.py".
+future development:
+ - more smooth plot update: maybe by "matpltlib.animation" or "CameraDisplay.update".
+ - a unified configutation
+ - a smarter normalization
+"""
+
+import glob
+import datetime
+import numpy as np
+from matplotlib import pyplot as plt
+from sclusco import ClusCoLog
+
+
+def fill_monitor(
+    fig, clusco_state,
+):
+
+    # config
+    # ToDo: relocate to main, automatic ncosl/nrows
+    ncols = 3
+    nrows = 2
+    idx = -1 # -1 or np.random.randint(int)
+    
+    # subplots
+    axes = fig.subplots(nrows=nrows, ncols=ncols)
+    displays = [
+        # High Voltage
+        clusco_state.high_voltage.show_snapshot(
+            ax=axes[0,0], idx=idx, 
+            # norm = mpl.colors.Normalize(vmin=0, vmax=1, clip=True), # ToDo: not work?
+        ),
+        # Anode Current
+        clusco_state.anode_current.show_snapshot(ax=axes[0,1], idx=idx),
+        # Amplifier Temperature
+        clusco_state.amp_temp.show_snapshot(ax=axes[0,2], idx=idx),
+        # L0 Rate
+        clusco_state.l0_rate.show_snapshot(ax=axes[1,0], idx=idx),
+        # L1 Rate
+        clusco_state.l1_rate.show_snapshot(ax=axes[1,1], idx=idx),
+        # Camera Rate
+        clusco_state.camera_rate.show_snapshot(ax=axes[1,2], idx=idx),
+    ]
+    # adjustment
+    fig.tight_layout()
+
+    return fig, axes, displays
+
+
+def monitor(
+    fig, path_clog, timestamp,
+):
+    
+    # read the ClusCo log
+    filenames = sorted(glob.glob(path_clog))
+    filename = filenames[-1]
+    log_clusco = ClusCoLog(filename=filename)
+    # read the status
+    clusco_states = log_clusco.states
+    fig.suptitle(
+        f"{timestamp.strftime('%c')}"
+        "\n"
+        f"{filename}"
+    )
+    # make monitor window
+    fig, axes, displays = fill_monitor(fig, clusco_states)
+    print(timestamp)
+
+    return fig, axes, displays
+
+
+
+def main():
+
+    max_duration = datetime.timedelta(minutes=30)
+    frame_time = 5.0 # seconds
+    path_clog = "./data/*.txt"
+
+    # start/end time
+    start = datetime.datetime.now()
+    expected_end = start + max_duration
+    # figure object
+    fig = plt.figure(figsize=(13,8))
+    fig.show()
+    # initial plot
+    fig, axes, displays = monitor(fig, path_clog, datetime.datetime.now())
+    plt.pause(frame_time)
+
+    timestamp = datetime.datetime.now()
+    while timestamp < expected_end:
+
+        # reset plot
+        fig.clf()
+        # read the latest file
+        fig, axes, displays = monitor(fig, path_clog, timestamp)
+        plt.pause(frame_time)
+        # time stamp update
+        timestamp = datetime.datetime.now()
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ sclusco.py
@@ -0,0 +1,393 @@
+#!/usr/bin/env python
+
+import numpy as np
+import datetime
+from ctapipe.instrument import CameraGeometry
+from ctapipe.visualization import CameraDisplay
+
+# to convert the camera geometry
+pix_spiral = np.load("pixel_spiral_id.npy")
+revert_spiral = np.argsort(pix_spiral)
+
+
+
+class States:
+
+    def __init__(
+        self, 
+        images, label,
+        geom = CameraGeometry.from_name("LSTCam-002"),
+        vmin = -np.inf, vmax = np.inf
+    ):
+        
+        self._images = images
+        self.label = label
+        self.geom = geom
+        self.vmin = vmin
+        self.vmax = vmax
+        self._images_clip = np.clip(self._images, self.vmin, self.vmax)
+
+
+
+class PixelStates(States):
+
+    def __init__(
+        self,
+        images, label,
+        vmin = -np.inf, vmax = np.inf,
+        cmin = None, cmax = None,
+        geom = CameraGeometry.from_name("LSTCam-002")
+    ):
+        
+        super().__init__(images, label, geom, vmin, vmax)
+        # conversion
+        self.images = self._images[revert_spiral, :]
+        self.images_clip = np.clip(self.images, vmin, vmax)
+        # for plot
+        self.cmin = cmin
+        self.cmax = cmax
+        self.display = None
+
+
+    def adjust_camera_image(self):
+
+        # self.display.add_frame_name=''
+        self.display.axes.set_aspect("equal")
+        self.display.axes.spines['top'].set_visible(False)
+        self.display.axes.spines['right'].set_visible(False)
+        self.display.axes.set_xticks([-1, -0.5, 0, 0.5, 1], ha='center', rotation=0)
+        self.display.axes.set_yticks([-1, -0.5, 0, 0.5, 1], va='center', rotation=90)
+        self.display.axes.set_xlabel(r'$x$ [m]')
+        self.display.axes.set_ylabel(r'$y$ [m]')
+        self.display.axes.set_xlim(-1.3, 1.3)
+        self.display.axes.set_ylim(-1.3, 1.3)
+        self.display.axes.grid(color='black', alpha=0.4, linestyle='dashed')
+    
+
+    def show_snapshot(
+        self, ax,
+        idx = -1,
+        adjust_plot = True,
+        *args, **kwargs,
+    ):
+
+        # ToDo: Replace "image_clip" with something like matplotlib.colors.Normalize
+        disp = CameraDisplay(
+            geometry = self.geom, 
+            image = self.images_clip[:, idx],
+            ax = ax, show_frame = False,
+            title = self.label,
+            *args, **kwargs,
+        )
+        # colorbar
+        disp.pixels.set_clim(self.cmin, self.cmax)
+        disp.add_colorbar(ax=ax) # label = self.label
+        # class variable
+        self.display = disp
+        # better visualization
+        if adjust_plot: self.adjust_camera_image()
+
+        return disp
+    
+
+    def show_time_evolution(
+        self, ax
+    ):
+        raise NotImplementedError
+    
+
+
+class ModuleStates(PixelStates):
+    
+    def __init__(
+        self,
+        images, label,
+        vmin = -np.inf, vmax = np.inf,
+        cmin = None, cmax = None,
+        geom = CameraGeometry.from_name("LSTCam-002")
+    ):
+        
+        self.module_images = images
+        images = np.repeat(images, 7, axis=0)
+        super().__init__(
+            images, label,
+            vmin, vmax, 
+            cmin, cmax, geom,
+        )
+
+
+
+
+class ClusCoStates:
+
+    def __init__(
+        self,
+        high_voltage, anode_current, 
+        amp_temp, bp_temp,
+        scb_temp, scb_hum, 
+        l0_rate, l0_threshold, 
+        l1_rate, camera_rate,
+        l1_threshold_A, l1_threshold_B, l1_threshold_C,
+        hot_pixel,
+    ):
+        
+        self.scb_temp = ModuleStates(
+            images=scb_temp, 
+            cmin = 20, cmax = 40,
+            label="SCB Temperature $[\mathrm{C}^{\circ}]$"
+        )
+        self.scb_hum = ModuleStates(
+            images=scb_hum, 
+            label="SCB Humidity [%]"
+        )
+        self.amp_temp = PixelStates(
+            images=amp_temp, 
+            cmin = 20, cmax = 40,
+            label="Amplifier Temperature $[\mathrm{C}^{\circ}]$"
+        )
+        self.high_voltage = PixelStates(
+            images=high_voltage, 
+            cmin = 0, cmax = 1500,
+            label="High Voltage [V]"
+        )
+        self.anode_current = PixelStates(
+            images=anode_current, 
+            cmin = 0.0,
+            label="Anode Current [uA]"
+        )
+        self.bp_temp = ModuleStates(
+            images=bp_temp,
+            cmin = 20, cmax = 40,
+            label="Back-Plane Temperature $[\mathrm{C}^{\circ}]$"
+        )
+        self.l0_rate = PixelStates(images=l0_rate, label="L0 Rate [Hz]")
+        self.l0_threshold = PixelStates(images=l0_threshold, label="L0 Threshold")
+        self.l1_rate = ModuleStates(images=l1_rate, label="L1 Rate [Hz]")
+        self.camera_rate = ModuleStates(images=camera_rate, label="Camera Rate [Hz]")
+        # self.l1_threshold_A = ModuleStates(images=l1_threshold_A, label="foo")
+        # self.l1_threshold_B = ModuleStates(images=l1_threshold_B, label="foo")
+        # self.l1_threshold_C = ModuleStates(images=l1_threshold_C, label="foo")
+        # self.hot_pixel = ModuleStates(images=hot_pixel, label="foo")
+    
+
+
+class ClusCoLog:
+
+    def __init__(
+        self, filename,
+        version = 1,
+        header = 9, 
+        unit = 115 # 90
+    ):
+
+        header = 9
+        unit = 115
+        if version == 0: unit = 90
+
+        self.info_col = self.make_info_col(version, header, unit)
+        self.filename = filename
+        self.data = np.loadtxt(self.filename, unpack=True, usecols=self.info_col)
+        self.time = self.read_time()
+        self.states = self.read_var()
+        self.data_header = 6 # ???
+        self.data_unit = 53 # ???
+
+
+    def make_info_col(
+        self, version, header, unit
+    ):
+
+        info_col = np.empty(0, int)
+        info_col = np.append(info_col, (2,3,4,5,6,7)) # time
+
+        for imod in range(265):
+            info_col = np.append(info_col, header+unit*imod+6) # SCB TEMP
+            info_col = np.append(info_col, header+unit*imod+8) # SCB HUM
+    
+            info_col = np.append(info_col, header+unit*imod+10) # AMP TEMP0
+            info_col = np.append(info_col, header+unit*imod+11) # AMP TEMP1
+            info_col = np.append(info_col, header+unit*imod+12) # AMP TEMP2
+            info_col = np.append(info_col, header+unit*imod+13) # AMP TEMP3
+            info_col = np.append(info_col, header+unit*imod+14) # AMP TEMP4
+            info_col = np.append(info_col, header+unit*imod+15) # AMP TEMP5
+            info_col = np.append(info_col, header+unit*imod+16) # AMP TEMP6
+            
+            info_col = np.append(info_col, header+unit*imod+60) # HV0
+            info_col = np.append(info_col, header+unit*imod+61) # HV1
+            info_col = np.append(info_col, header+unit*imod+62) # HV2
+            info_col = np.append(info_col, header+unit*imod+63) # HV3
+            info_col = np.append(info_col, header+unit*imod+64) # HV4
+            info_col = np.append(info_col, header+unit*imod+65) # HV5
+            info_col = np.append(info_col, header+unit*imod+66) # HV6
+            
+            
+            info_col = np.append(info_col, header+unit*imod+68) # Anode current0
+            info_col = np.append(info_col, header+unit*imod+69) # Anode current1
+            info_col = np.append(info_col, header+unit*imod+70) # Anode current2
+            info_col = np.append(info_col, header+unit*imod+71) # Anode current3
+            info_col = np.append(info_col, header+unit*imod+72) # Anode current4
+            info_col = np.append(info_col, header+unit*imod+73) # Anode current5
+            info_col = np.append(info_col, header+unit*imod+74) # Anode current6
+            
+            info_col = np.append(info_col, header+unit*imod+76) # BP TEMP
+            
+            info_col = np.append(info_col, header+unit*imod+79) # IPR0
+            info_col = np.append(info_col, header+unit*imod+80) # IPR1
+            info_col = np.append(info_col, header+unit*imod+81) # IPR2
+            info_col = np.append(info_col, header+unit*imod+82) # IPR3
+            info_col = np.append(info_col, header+unit*imod+83) # IPR4
+            info_col = np.append(info_col, header+unit*imod+84) # IPR5
+            info_col = np.append(info_col, header+unit*imod+85) # IPR6
+        
+            if(version==0):
+
+                info_col = np.append(info_col, header+unit*imod+87) # L1 local trigger rate
+                info_col = np.append(info_col, header+unit*imod+89) # camera trigger rate
+            
+            elif(version==1):
+            
+                info_col = np.append(info_col, header+unit*imod+88) # L0 threshold0
+                info_col = np.append(info_col, header+unit*imod+89) # L0 threshold1
+                info_col = np.append(info_col, header+unit*imod+90) # L0 threshold2
+                info_col = np.append(info_col, header+unit*imod+91) # L0 threshold3
+                info_col = np.append(info_col, header+unit*imod+92) # L0 threshold4
+                info_col = np.append(info_col, header+unit*imod+93) # L0 threshold5
+                info_col = np.append(info_col, header+unit*imod+94) # L0 threshold6
+                
+                info_col = np.append(info_col, header+unit*imod+96) # L1 local trigger rate
+                info_col = np.append(info_col, header+unit*imod+98) # camera trigger rate
+                
+                info_col = np.append(info_col, header+unit*imod+101) # L1 threshold0
+                info_col = np.append(info_col, header+unit*imod+102) # L1 threshold1
+                info_col = np.append(info_col, header+unit*imod+103) # L1 threshold2
+                info_col = np.append(info_col, header+unit*imod+104) # L1 threshold3
+                info_col = np.append(info_col, header+unit*imod+105) # L1 threshold4
+                info_col = np.append(info_col, header+unit*imod+106) # L1 threshold5
+                
+                info_col = np.append(info_col, header+unit*imod+108) # hotpix0
+                info_col = np.append(info_col, header+unit*imod+109) # hotpix1
+                info_col = np.append(info_col, header+unit*imod+110) # hotpix2
+                info_col = np.append(info_col, header+unit*imod+111) # hotpix3
+                info_col = np.append(info_col, header+unit*imod+112) # hotpix4
+                info_col = np.append(info_col, header+unit*imod+113) # hotpix5
+                info_col = np.append(info_col, header+unit*imod+114) # hotpix6
+
+        return info_col
+
+
+    def read_time(self):
+
+        time_=[[] for i in range(6)]
+        time=[]
+
+        for i in range(6):
+            time_[i] = np.array(self.data[i], dtype='int')
+            
+        for i in range(np.size(time_[0])):
+            datetime_ = datetime.datetime(time_[0][i],time_[1][i],time_[2][i],time_[3][i],time_[4][i],time_[5][i], tzinfo=datetime.timezone.utc)
+            time = np.append(time, datetime_)
+        
+        return time
+    
+
+    def read_var(self):
+
+        temp_scb_ = [[] for i in range(265)]
+        humi_scb_ = [[] for i in range(265)]
+        temp_amp_ = [[] for i in range(1855)]
+        hv_ = [[] for i in range(1855)]
+        an_cur_ = [[] for i in range(1855)]
+        temp_bp_ = [[] for i in range(265)]
+        l0_rate_ = [[] for i in range(1855)]
+        l0_threshold_ = [[] for i in range(1855)]
+        l1_rate_ = [[] for i in range(265)]
+        camera_rate_ = [[] for i in range(265)]
+        l1_threshold_A_ = [[] for i in range(265)]
+        l1_threshold_B_ = [[] for i in range(265)]
+        l1_threshold_C_ = [[] for i in range(265)]
+        hot_pixel_ = [[] for i in range(1855)]
+        
+        for imod in range(265):    
+            ## -- Temperature (SCB) -- ##
+            temp_scb_[imod] = self.data[self.data_header + 0 + self.data_unit * imod] #8-14  
+            
+            ## -- Humidity (SCB) -- ##
+            humi_scb_[imod] = self.data[self.data_header + 1 + self.data_unit * imod] #8-14
+
+            ## -- Temperature(PACTA board) -- ##
+            for ipix in range(7):
+                temp_amp_[imod*7+ipix] = self.data[self.data_header + 2 + self.data_unit*imod + ipix] #8-14
+
+            ## --  HV --- ##
+            for ipix in range(7):
+                hv_[imod*7+ipix] = self.data[self.data_header + 9 + self.data_unit * imod + ipix] #15-21 
+                
+            ## --  anode current --- ##
+            for ipix in range(7):
+                an_cur_[imod*7+ipix] = self.data[self.data_header + 16 + self.data_unit * imod + ipix]
+
+            ## -- Temperature (SCB) -- ##
+            temp_bp_[imod] = self.data[self.data_header + 23 + self.data_unit * imod] #8-14  
+    
+            ## --  L0 rate --- ##
+            for ipix in range(7):
+                l0_rate_[imod*7+ipix] = self.data[self.data_header + 24 + self.data_unit * imod + ipix] #30-36    
+
+            ## --  L0 threshold -- ##
+            for ipix in range(7):
+                l0_threshold_[imod*7+ipix] = self.data[self.data_header + 31 + self.data_unit * imod + ipix] #37-43    
+
+            ## --  L1 rate --- ##
+            l1_rate_[imod] = self.data[self.data_header + 38 + self.data_unit * imod] #44  
+    
+            ## --  camera rate --- ##
+            camera_rate_[imod] = self.data[self.data_header + 39 + self.data_unit * imod] #45 
+    
+            ## --  L1 threshold -- ##             
+            l1_threshold_A_[imod] = self.data[self.data_header + 40 + self.data_unit*imod]/4 #46-51   
+            l1_threshold_B_[imod] = self.data[self.data_header + 41 + self.data_unit*imod]/4 #46-51
+            l1_threshold_C_[imod] = self.data[self.data_header + 42 + self.data_unit*imod]/4 #46-51
+    
+            ## --  hot_pixel -- ##
+            for ipix in range(7):
+                hot_pixel_[imod*7+ipix] = self.data[self.data_header + 46 + self.data_unit * imod + ipix] #52-58    
+        
+    
+        scb_temp = np.vstack(temp_scb_)
+        scb_hum = np.vstack(humi_scb_)
+        amp_temp = np.vstack(temp_amp_)
+        high_voltage = np.vstack(hv_)
+        anode_current = np.vstack(an_cur_)
+        bp_temp = np.vstack(temp_bp_)
+        l0_rate = np.vstack(l0_rate_)
+        l0_threshold = np.vstack(l0_threshold_)
+        l1_rate = np.vstack(l1_rate_)
+        camera_rate = np.vstack(camera_rate_)
+        l1_threshold_A = np.vstack(l1_threshold_A_)
+        l1_threshold_B = np.vstack(l1_threshold_B_)
+        l1_threshold_C = np.vstack(l1_threshold_C_)
+        hot_pixel = np.vstack(hot_pixel_)
+
+
+        ####################################
+        ### overwritten below by Shotaro ###
+        ####################################
+
+        clusco_states = ClusCoStates(
+            scb_temp = scb_temp,
+            scb_hum = scb_hum,
+            amp_temp = amp_temp,
+            high_voltage = high_voltage,
+            anode_current = anode_current,
+            bp_temp = bp_temp,
+            l0_rate = l0_rate,
+            l0_threshold = l0_threshold,
+            l1_rate = l1_rate,
+            camera_rate = camera_rate,
+            l1_threshold_A = l1_threshold_A,
+            l1_threshold_B = l1_threshold_B,
+            l1_threshold_C = l1_threshold_C,
+            hot_pixel = hot_pixel,
+        )
+        
+        return clusco_states
